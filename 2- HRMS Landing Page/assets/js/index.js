ac326160bb928e01a7d2c4342e85176f73d6e2fb
Muhammed Zohaib <alizuhaib828@gmail.com>
2024-03-02 01:58:33 +0500

    Added hero-section for landing page

--- 2- HRMS Landing Page/assets/js/index.js
+++ 2- HRMS Landing Page/assets/js/index.js
@@ -1,6 +1,9 @@
 const navOpen = document.querySelector(".open");
 const navClose = document.querySelector(".close"); 
 const navbarDiv = document.querySelector(".navigation"); 
+const heroSectionImage = document.querySelector(".hero-section-image"); 
+const imagePath_desktop = "./assets/images/illustrations/rocket_desktop.svg";
+const imagePath_mobile= "./assets/images/illustrations/rocket_mobile.svg";
 
 
 
@@ -18,4 +21,13 @@ navClose.addEventListener("click", ()=>{
     navClose.classList.add("open-close");
     navbarDiv.classList.remove("open-navigation");
     navbarDiv.classList.add("hide");
+})
+
+window.addEventListener("resize", ()=>{
+    if(window.innerWidth >= 768){
+        heroSectionImage.setAttribute("src", imagePath_desktop)
+    }
+    else{
+        heroSectionImage.setAttribute("src", imagePath_mobile)
+    }
 })
